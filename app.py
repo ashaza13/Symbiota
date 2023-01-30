a6320590c274e5090242c73c7c64626346da1fbb
AshazA <94416376+ashaza13@users.noreply.github.com>
2023-01-30 12:30:43 -0500

    Completed Banner

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import os
 
 from flask import Flask, flash, redirect, render_template, request, session, url_for
+from logging import FileHandler, WARNING
 
 app = Flask(__name__, template_folder="templates")
 
-@app.route("/")
+file_handler = FileHandler('errorlog.txt')
+file_handler.setLevel(WARNING)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
